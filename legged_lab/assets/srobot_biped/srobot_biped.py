b4c7e4a22096290a34093764777c727a9aa8ff61
Zhang Yf <759094438@qq.com>
2025-03-12 21:32:53 +0800

    [FIX] fix path typo [ADD] begin add srobot custom config

---
+++ legged_lab/assets/srobot_biped/srobot_biped.py
@@ -0,0 +1,68 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import isaaclab.sim as sim_utils
+from isaaclab.actuators import ImplicitActuatorCfg
+from isaaclab.assets.articulation import ArticulationCfg
+from legged_lab.assets import ISAAC_ASSET_DIR
+
+SROBOT_BIPED_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=f"{ISAAC_ASSET_DIR}/srobot-biped/usd/srobot-biped.usd",
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=True, solver_position_iteration_count=4, solver_velocity_iteration_count=4
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 1.05),
+        joint_pos={
+            ".*_hip_pitch.*": -0.28,
+            ".*_knee.*": 0.79,
+            ".*_ankle.*": -0.52,
+            ".*_shoulder_pitch.*": 0.20,
+            ".*_elbow.*": 0.32,
+        },
+        joint_vel={".*": 0.0},
+    ),
+    soft_joint_pos_limit_factor=0.9,
+    actuators={
+        "legs": ImplicitActuatorCfg(
+            joint_names_expr=[".*_hip_yaw.*", ".*_hip_roll.*", ".*_hip_pitch.*", ".*_knee.*", ".*torso.*"],
+            effort_limit_sim=300.0,
+            velocity_limit_sim=100.0,
+            stiffness={
+                ".*_hip_yaw.*": 200.0,
+                ".*_hip_roll.*": 200.0,
+                ".*_hip_pitch.*": 200.0,
+                ".*_knee.*": 300.0,
+                ".*torso.*": 300.0,
+            },
+            damping={
+                ".*_hip_yaw.*": 5.0,
+                ".*_hip_roll.*": 5.0,
+                ".*_hip_pitch.*": 5.0,
+                ".*_knee.*": 6.0,
+                ".*torso.*": 6.0,
+            },
+        ),
+        "feet": ImplicitActuatorCfg(
+            joint_names_expr=[".*_ankle.*"],
+            effort_limit_sim=100.0,
+            velocity_limit_sim=100.0,
+            stiffness={".*_ankle.*": 40.0},
+            damping={".*_ankle.*": 2.0},
+        ),
+    },
+)
---
+++ legged_lab/envs/srobot/srobot_biped_config.py
@@ -0,0 +1,120 @@
+from legged_lab.envs.base.base_env_config import (  # noqa:F401
+    BaseEnvCfg,
+    BaseAgentCfg,
+    BaseSceneCfg,
+    RobotCfg,
+    DomainRandCfg,
+    RewardCfg,
+    HeightScannerCfg,
+    AddRigidBodyMassCfg,
+    PhysxCfg,
+    SimCfg,
+    MLPPolicyCfg,
+    RNNPolicyCfg,
+)
+
+# from legged_lab.assets.unitree import G1_CFG
+from legged_lab.assets.srobot_biped import SROBOT_BIPED_CFG
+from legged_lab.terrains import GRAVEL_TERRAINS_CFG, ROUGH_TERRAINS_CFG
+from isaaclab.managers import RewardTermCfg as RewTerm
+import legged_lab.mdp as mdp
+from isaaclab.managers.scene_entity_cfg import SceneEntityCfg
+from isaaclab.utils import configclass
+
+
+@configclass
+class SrobotBipedRewardCfg(RewardCfg):
+    track_lin_vel_xy_exp = RewTerm(func=mdp.track_lin_vel_xy_yaw_frame_exp, weight=1.0, params={"std": 0.5})
+    track_ang_vel_z_exp = RewTerm(func=mdp.track_ang_vel_z_world_exp, weight=1.0, params={"std": 0.5})
+    lin_vel_z_l2 = RewTerm(func=mdp.lin_vel_z_l2, weight=-1.0)
+    ang_vel_xy_l2 = RewTerm(func=mdp.ang_vel_xy_l2, weight=-0.05)
+    energy = RewTerm(func=mdp.energy, weight=-1e-3)
+    dof_acc_l2 = RewTerm(func=mdp.joint_acc_l2, weight=-2.5e-7)
+    action_rate_l2 = RewTerm(func=mdp.action_rate_l2, weight=-0.01)
+    undesired_contacts = RewTerm(
+        func=mdp.undesired_contacts, weight=-1.0, params={"sensor_cfg": SceneEntityCfg("contact_sensor", body_names="(?!.*ankle.*).*"), "threshold": 1.0}
+    )
+    fly = RewTerm(func=mdp.fly, weight=-1.0, params={"sensor_cfg": SceneEntityCfg("contact_sensor", body_names=".*ankle_roll.*"), "threshold": 1.0})
+    body_orientation_l2 = RewTerm(func=mdp.body_orientation_l2, params={"asset_cfg": SceneEntityCfg("robot", body_names=".*torso.*")}, weight=-2.0)
+    flat_orientation_l2 = RewTerm(func=mdp.flat_orientation_l2, weight=-1.0)
+    termination_penalty = RewTerm(func=mdp.is_terminated, weight=-200.0)
+    feet_air_time = RewTerm(
+        func=mdp.feet_air_time_positive_biped,
+        weight=0.15,
+        params={"sensor_cfg": SceneEntityCfg("contact_sensor", body_names=".*ankle_roll.*"), "threshold": 0.4},
+    )
+    feet_slide = RewTerm(
+        func=mdp.feet_slide,
+        weight=-0.25,
+        params={
+            "sensor_cfg": SceneEntityCfg("contact_sensor", body_names=".*ankle_roll.*"),
+            "asset_cfg": SceneEntityCfg("robot", body_names=".*_ankle_roll.*"),
+        },
+    )
+    feet_force = RewTerm(
+        func=mdp.body_force,
+        weight=-3e-3,
+        params={"sensor_cfg": SceneEntityCfg("contact_sensor", body_names=".*ankle_roll.*"), "threshold": 500, "max_reward": 400},
+    )
+    feet_too_near = RewTerm(
+        func=mdp.feet_too_near_humanoid, weight=-2.0, params={"asset_cfg": SceneEntityCfg("robot", body_names=[".*ankle_roll.*"]), "threshold": 0.2}
+    )
+    feet_stumble = RewTerm(func=mdp.feet_stumble, weight=-2.0, params={"sensor_cfg": SceneEntityCfg("contact_sensor", body_names=[".*ankle_roll.*"])})
+    dof_pos_limits = RewTerm(func=mdp.joint_pos_limits, weight=-2.0)
+    joint_deviation_hip = RewTerm(
+        func=mdp.joint_deviation_l1,
+        weight=-0.15,
+        params={"asset_cfg": SceneEntityCfg("robot", joint_names=[".*_hip_yaw.*", ".*_hip_roll.*", ".*_shoulder_pitch.*", ".*_elbow.*"])},
+    )
+    joint_deviation_arms = RewTerm(
+        func=mdp.joint_deviation_l1,
+        weight=-0.2,
+        params={"asset_cfg": SceneEntityCfg("robot", joint_names=[".*waist.*", ".*_shoulder_roll.*", ".*_shoulder_yaw.*", ".*_wrist.*"])},
+    )
+    joint_deviation_legs = RewTerm(
+        func=mdp.joint_deviation_l1, weight=-0.02, params={"asset_cfg": SceneEntityCfg("robot", joint_names=[".*_hip_pitch.*", ".*_knee.*", ".*_ankle.*"])}
+    )
+
+
+@configclass
+class SrobotBipedFlatEnvCfg(BaseEnvCfg):
+    scene = BaseSceneCfg(
+        height_scanner=HeightScannerCfg(enable_height_scan=False, prim_body_name="torso_link"),
+        robot=SROBOT_BIPED_CFG,
+        terrain_type="generator",
+        terrain_generator=GRAVEL_TERRAINS_CFG,
+    )
+    robot = RobotCfg(terminate_contacts_body_names=[".*torso.*"], feet_body_names=[".*ankle_roll.*"])
+    domain_rand = DomainRandCfg(
+        add_rigid_body_mass=AddRigidBodyMassCfg(enable=True, params={"body_names": [".*torso.*"], "mass_distribution_params": (-5.0, 5.0), "operation": "add"})
+    )
+    reward = SrobotBipedRewardCfg()
+
+
+@configclass
+class SrobotBipedFlatAgentCfg(BaseAgentCfg):
+    experiment_name: str = "g1_flat"
+    wandb_project: str = "g1_flat"
+
+
+@configclass
+class SrobotBipedRoughEnvCfg(SrobotBipedFlatEnvCfg):
+    scene = BaseSceneCfg(
+        height_scanner=HeightScannerCfg(enable_height_scan=True, prim_body_name="torso_link"),
+        robot=SROBOT_BIPED_CFG,
+        terrain_type="generator",
+        terrain_generator=ROUGH_TERRAINS_CFG,
+    )
+    robot = RobotCfg(actor_obs_history_length=1, critic_obs_history_length=1, terminate_contacts_body_names=[".*torso.*"], feet_body_names=[".*ankle_roll.*"])
+    reward = SrobotBipedRewardCfg(
+        track_lin_vel_xy_exp=RewTerm(func=mdp.track_lin_vel_xy_yaw_frame_exp, weight=1.5, params={"std": 0.5}),
+        track_ang_vel_z_exp=RewTerm(func=mdp.track_ang_vel_z_world_exp, weight=1.5, params={"std": 0.5}),
+        lin_vel_z_l2=RewTerm(func=mdp.lin_vel_z_l2, weight=-0.25),
+    )
+
+
+@configclass
+class SrobotBipedRoughAgentCfg(BaseAgentCfg):
+    experiment_name: str = "srobot_biped_rough"
+    wandb_project: str = "srobot_biped_rough"
+    policy = RNNPolicyCfg()
